1fd19a4c99530046564d520dc7ad665487d5eae4
maxi <maximiliano.e.chaves@gmail.com>
2021-09-01 13:37:59 -0300

    actualizo servidor para cargar css y js

--- lista todo/server.js
+++ lista todo/server.js
@@ -1,18 +1,31 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
-const PORT=8080; 
+http.createServer(function(req, res){
 
-fs.readFile('./index.html', function (err, html) {
+    if(req.url === "/"){
+        fs.readFile("./index.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
 
-    if (err) throw err;    
+    }else if(req.url.match("\.js$")){
+        var imagePath = path.join(__dirname, req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "javascript"});
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+}).listen(8080);
 
 var mysql = require('mysql');
 
@@ -23,7 +36,7 @@ var con = mysql.createConnection({
   database: "tododb"
 });
 
-
+	
 con.connect(function(err) {
   if (err) throw err;
   con.query("SELECT * FROM todo", function (err, result, fields) {
