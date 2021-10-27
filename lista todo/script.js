049a8708fd6ad94d00591da124b89abdfd553ced
maximiliano <maximiliano.e.chaves@gmail.com>
2021-10-26 22:52:51 -0300

    agrego funcion para escribir items

--- lista todo/script.js
+++ lista todo/script.js
@@ -1,11 +1,15 @@
+//const { json } = require("stream/consumers")
+
 const boton = document.querySelector(".todo-button")
 const datos = document.querySelector(".todo-input")
 const lista = document.querySelector(".todo-list")
 
 boton.addEventListener("click", agregar)
 
+//agrega el item a la todo
 function agregar(e){
 	e.preventDefault()
+	test()
 	const item = document.createElement("li")
 	const boton = document.createElement("button")
 	boton.classList.add("delete-button")
@@ -16,3 +20,30 @@ function agregar(e){
 	}
 	datos.value=""
 }
+
+async function getUsers() {
+    let url = 'datos.json';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderUsers() {
+    let users = await getUsers();
+    let html = '';
+    users.forEach(user => {
+        let htmlSegment = `<div class="user">
+                            <h2>${user.contenido}</h2>
+                        </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.todo-list');
+    container.innerHTML = html;
+}
+
+renderUsers();
