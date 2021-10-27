b37fab650905b3a1b7ed448a12cc88f546ad4d8d
maximiliano <maximiliano.e.chaves@gmail.com>
2021-10-26 22:55:47 -0300

    servidor refactorizado

--- lista todo/server.js
+++ lista todo/server.js
@@ -4,89 +4,38 @@ var path = require('path');
 let mail = "maxi@gmail.com"
 //creamos el server y esperamos el post desde el login
 http.createServer(function (req, res) {
-
-    /* switch (req.url) {
-        case "/":
-            fs.readFile("./login.html", "UTF-8", function(err, html){
-                res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"});
-                res.end(html);
-            });
-            break;
-        case (req.url.match("\.css$")):
-            var cssPath = path.join(__dirname, req.url);
-            var fileStream = fs.createReadStream(cssPath, "UTF-8");
-            res.writeHead(200, {"Content-Type": "text/css"});
-            fileStream.pipe(res)
-            break;
-        case (req.url.match("\.js$")):
-            var imagePath = path.join(__dirname, req.url);
-            var fileStream = fs.createReadStream(imagePath);
-            res.writeHead(200, {"Content-Type": "javascript"});
-            fileStream.pipe(res);
-            break;
-        case (req.url.match("\.index.html")):
-            fs.readFile("./index.html", "UTF-8", function(err, html){
-                res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"});
-                res.end(html);
-            });
-            break;
-        case (req.method === 'POST'):
-            let body = '';
-            req.on('data', chunk => {
-                body += chunk.toString(); // convert Buffer to string
-            });
-            req.on('end', () => {
-                console.log(body);
-                let dato = body.split('&')
-                let usuario = dato[0].split('=')
-                let pass = dato[1].split('=')
-                usuario = usuario[1]
-                pass = pass[1]
-                usuario + decodeURIComponent(usuario.replace('/'))
-                con.connect(function(err) {
-                    if (err) throw err;
-                    con.query("select usuario.mail, todo.id, item.id_todo,item.contenido from usuario inner join todo on '"+mail+"' =todo.mail_user inner join item on todo.id =item.id_todo", function (err, result, fields) {
-                      if (err) throw err;
-                      console.log(result);
-                    });
-                  });
-            });
-            break;
-        default:
-            res.writeHead(404, {"Content-Type": "text/html"});
-            res.end("No Page Found");
-            break;
-    } */
-    /* fs.readFile("./index.html", "UTF-8", function(err, index){
-        res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"});
-        res.end(index);
-    }); */
+    //carga la pagina del login
     if (req.url === "/") {
         fs.readFile("./login.html", "UTF-8", function (err, html) {
             res.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
             res.end(html);
         });
+        //carga el css
     } else if (req.url.match("\.css$")) {
         var cssPath = path.join(__dirname, req.url);
         var fileStream = fs.createReadStream(cssPath, "UTF-8");
         res.writeHead(200, { "Content-Type": "text/css" });
         fileStream.pipe(res);
-
+        //carga el javascript
     } else if (req.url.match("\.js$")) {
         var imagePath = path.join(__dirname, req.url);
         var fileStream = fs.createReadStream(imagePath);
         res.writeHead(200, { "Content-Type": "javascript" });
         fileStream.pipe(res);
-    }else if (req.url.match("\.datos.json")) {
-            var imagePath = path.join(__dirname, req.url);
-            var fileStream = fs.createReadStream(imagePath);
-            res.writeHead(200, { "Content-Type": "application/json" });
-            fileStream.pipe(res);
+        //carga el archivo con los datos de usuario
+    } else if (req.url.match("\.datos.json")) {
+        var imagePath = path.join(__dirname, req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        fileStream.pipe(res);
+        //carga la pagina index
     } else if (req.url.match("\.index.html")) {
+        //si es un post, consigo los datos del usuario y hago una query a la base de datos
+        //para obtener sus to-dos
         if (req.method === 'POST') {
             let body = '';
             req.on('data', chunk => {
-                body += chunk.toString(); // convert Buffer to string
+                body += chunk.toString();
             });
             req.on('end', () => {
                 console.log(body);
@@ -98,68 +47,44 @@ http.createServer(function (req, res) {
                 usuario = decodeURIComponent(usuario.replace(/\+/g, ' '));
                 console.log(usuario)
                 con.connect(function (err) {
-                    //if (err) throw err;
-                    con.query("select usuario.mail, todo.id, item.id_todo,item.contenido from usuario inner join todo on '" + usuario + "' =todo.mail_user and '" + usuario + "'= usuario.mail inner  join item on todo.id =item.id_todo", function (err, result, fields) {
-                        if (result == '') {
-                            console.log("no esta")
-                            //con.query("insert into usuario values ('"+usuario+"', 'sososo');")
-                        }
-                        else {
-                            console.log(result)
-                            let contenido=JSON.stringify(result)
-                            console.log(contenido)
-                            fs.writeFile('datos.json', contenido, (err) => {
-                                if (err) throw err;
-                                console.log('Data written to file');
-                            });
-                            //res.json(result)
-                            /* res.writeHead(200, { 'Content-Type': 'application/json' });
-                            res.write(JSON.stringify({
-                                data: 'Hello World!'
-                            }));
-                            res.end("ola") */
-                        } 
-                    });
+                    if (err) throw err;
                 });
-                
+                let contenido
+                con.query("select mail from usuario where mail ='" + usuario + "'", function (err, result, fields) {
+
+                    console.log(result)
+                    if (result == '') {
+                        //si la query llega vacia, el usuario no existe y lo creo
+                        console.log("no esta")
+                        con.query("insert into usuario values ('" + usuario + "', '" + pass + "');")
+                        contenido = ''
+                    }
+                });
+                //la query no llega vacia, hago la query completa y guardo en json
+                con.query("select item.contenido from usuario inner join todo on '" + usuario + "' =todo.mail_user and '" + usuario + "'= usuario.mail inner  join item on todo.id =item.id_todo", function (err, result, fields) {
+                    console.log(result)
+                    //aca a la variable contenido tengo que darle el json de result
+                    //y guardar ese contenido en un archivo que se llame
+                    //datos.json
+                });
             });
         }
         fs.readFile("./index.html", "UTF-8", function (err, html) {
-            
-            res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8" });
+
+            res.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
             res.end(html);
         });
+        //404 caso contrario
     } else {
         res.writeHead(404, { "Content-Type": "text/html" });
         res.end("No Page Found");
     }
-    /* if (req.method === 'POST') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString(); // convert Buffer to string
-        });
-        req.on('end', () => {
-            console.log(body);
-            let dato = body.split('&')
-            let usuario = dato[0].split('=')
-            let pass = dato[1].split('=')
-            usuario = usuario[1]
-            pass = pass[1]
-            usuario + decodeURIComponent(usuario.replace('/'))
-            con.connect(function(err) {
-                if (err) throw err;
-                con.query("select usuario.mail, todo.id, item.id_todo,item.contenido from usuario inner join todo on '"+mail+"' =todo.mail_user inner join item on todo.id =item.id_todo", function (err, result, fields) {
-                  if (err) throw err;
-                  console.log(result);
-                });
-              });
-        });
 
-    } */
+
 }).listen(8080);
 
 
-//coneccion de prueba con la base de datos
+//conexion con la base de datos
 var mysql = require('mysql');
 const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants');
 const { getMaxListeners } = require('process');
@@ -171,15 +96,3 @@ var con = mysql.createConnection({
     database: "tododb"
 });
 
-
-//query de prueba
-/* con.connect(function(err) {
-  if (err) throw err;
-  con.query("select usuario.mail, todo.id, item.id_todo,item.contenido from usuario inner join todo on usuario.mail =todo.mail_user inner join item on todo.id =item.id_todo", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    console.log("hola"+mail+"chau")
-  });
-});
-
- */
