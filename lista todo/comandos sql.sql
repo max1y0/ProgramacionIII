c8f5945031ef196cb7571aa94229aca9e7e017f9
maximiliano <maximiliano.e.chaves@gmail.com>
2021-11-03 12:12:28 -0300

    actualizo comandos sql

--- lista todo/comandos sql.sql
+++ lista todo/comandos sql.sql
@@ -1,29 +1,111 @@
-create table usuario(
-    mail varchar(60) primary key,
-	contraseña varchar(60)
-);
-create table todo(
-    id serial primary key,
-    mail_user varchar(60),
-    foreign key (mail_user) references usuario(mail)
-);
-create table item(
-    id serial primary key,
-    contenido varchar(60),
-    borradosi_no varchar(60),
-    id_todo bigint unsigned,
-    foreign key (id_todo) references todo(id)
-);
-insert into usuario values ('sofi.lore1@gmail.com', 'sososo');
-insert into usuario values ('sole.esme2@gmail.com', 'lelele');
-insert into usuario values ('cande.cele3@gmail.com', 'cacaca');
-insert into usuario values ('ori.sil4@gmail.com', 'ororor');
-insert into todo(mail_user) values ('sofi.lore1@gmail.com');
-insert into todo(mail_user) values ('sole.esme2@gmail.com');
-insert into todo(mail_user) values ('cande.cele3@gmail.com');
-insert into todo(mail_user) values ('ori.sil4@gmail.com');
-insert into item(contenido,borradosi_no,id_todo) values ('matematica bien', 'no', 1);
-insert into item(contenido,borradosi_no,id_todo) values ('lengua bien', 'si', 2);
-insert into item(contenido,borradosi_no,id_todo) values ('fisica falta', 'no', 3);
-insert into item(contenido,borradosi_no,id_todo) values ('matematica mal', 'si', 4);
-select * from item;
+-- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: tododb
+-- ------------------------------------------------------
+-- Server version	8.0.26-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `contenido` varchar(60) DEFAULT NULL,
+  `borradosi_no` varchar(60) DEFAULT NULL,
+  `id_todo` bigint unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `id_todo` (`id_todo`),
+  CONSTRAINT `item_ibfk_1` FOREIGN KEY (`id_todo`) REFERENCES `todo` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'matematica bien','no',1),(2,'lengua bien','si',2),(3,'fisica falta','no',3),(4,'matematica mal','si',4);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `todo`
+--
+
+DROP TABLE IF EXISTS `todo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `todo` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `mail_user` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `mail_user` (`mail_user`),
+  CONSTRAINT `todo_ibfk_1` FOREIGN KEY (`mail_user`) REFERENCES `usuario` (`mail`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `todo`
+--
+
+LOCK TABLES `todo` WRITE;
+/*!40000 ALTER TABLE `todo` DISABLE KEYS */;
+INSERT INTO `todo` VALUES (3,'cande.cele3@gmail.com'),(4,'ori.sil4@gmail.com'),(1,'sofi.lore1@gmail.com'),(2,'sole.esme2@gmail.com');
+/*!40000 ALTER TABLE `todo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `mail` varchar(60) NOT NULL,
+  `contraseña` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES ('cande.cele3@gmail.com','cacaca'),('ori.sil4@gmail.com','ororor'),('sofi.lore1@gmail.com','sososo'),('sole.esme2@gmail.com','lelele');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-27 13:33:16
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+
+flush privileges;
