767be1c5ca5ce3146473beebe1c12b8d3852b9bc
Maximiliano Chaves <maximilianochaves@Maximilianos-MacBook-Air.local>
2022-05-15 21:17:02 -0300

    Actualizo con botones y nuevas funcionalidades a completar

--- poukemon/script.js
+++ poukemon/script.js
@@ -3,9 +3,32 @@ let poukemon = {
 	saciedad: 50, 
 	energia: 50, 
 	salud: 50,
+	animo: 50,
+	higuiene: 50,
+	nivel: 1,
+	cantCacas: 0,
 }
-console.log("holaaaa")
+
 function comer(){
 	poukemon.saciedad += 1
 	console.log(poukemon.saciedad)
+}
+
+function mimar(){
+	poukemon.animo += 1
+	console.log("mimando, animo en: " + poukemon.animo)
+}
+
+function baniar(){
+	poukemon.higuiene = 100
+	console.log("baño tomado, higuiene en: " + poukemon.higuiene)
+}
+
+function limpiar(){
+	poukemon.cantCacas = 0 
+	console.log("cacas limpiadas")
+}
+
+function guardar() {
+	// hacer un json con los datos del pou y la fecha y hora de guardado
 }
